65fb192c451bc6e412bc9617b13423e9a3836cbf
Dominic <andro5@web.de>
2024-10-09 11:12:49 +0200

    add security oAuth2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,38 @@
 
 import './App.css'
+import axios from "axios";
 
 function App() {
 
+    function getBook(){
+        axios.get("/api/book")
+            .then(response => console.log(response.data))
+    }
+
+    function getSecretBook(){
+        axios.get("/api/book/secret")
+            .then(response => console.log(response.data))
+    }
+
+    function login(){
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+
+        window.open(host+'/oauth2/authorization/github', '_self')
+    }
+
+    function getMe(){
+        axios.get("/api/auth/me/2")
+            .then(r => console.log(r.data))
+    }
 
   return (
     <>
       <h1>Hallo Test!</h1>
-        <p>Successful deploy</p>
+        <button onClick={getBook}>GetBook</button>
+        <button onClick={getSecretBook}>GetSecretBook</button>
+
+        <button onClick={login}>Login</button>
+        <button onClick={getMe}>Me</button>
     </>
   )
 }
